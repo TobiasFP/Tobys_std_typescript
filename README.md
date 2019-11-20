75935a5c5a5fbda60618e184450b6ccf67af9f31
Tobias <tobias@osandweb.dk>
2019-11-20 13:03:30 +0100

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Tobys_std_typescript
+
+To get started run 
+npm run build
+
+to run tests:
+npm run test
+
+to watch:
+Use tsc: watch, eg. by hitting ctrl + shift + b in vscode, and run the command
--- app/js/__tests__/greeter.test.js
+++ app/js/__tests__/greeter.test.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const classes_1 = require("../classes");
-test('My Greeter', () => {
-    expect(classes_1.Greeter('Carl')).toBe('Hello Carl');
+test("My Greeter", () => {
+    expect(classes_1.Greeter("Carl")).toBe("Hello Carl");
 });
